ef5ba44ff9e14684febfe9b9df33e205108c3ca9
multipolygon <multipolygon@gmail.com>
2020-09-14 19:34:12 +1000

    Use modified ssd1306 OLED driver.

--- src/app/solar_hws/display.py
+++ src/app/solar_hws/display.py
@@ -1,31 +1,36 @@
-from esp.esp8266.wemos.d1mini.oled import OLED
+from machine import I2C, Pin
+from lib.esp.esp8266.wemos.d1mini import pinmap
+from vendor.ssd1306 import SSD1306
 from utime import sleep
 import wifi
 
 class Display():
     def __init__(self, state):
         self.update_on = ('mqtt', 'solar_temp', 'tank_temp', 'tank_target_temp', 'mode', 'pump')
-        self.oled = OLED()
-        self.mqtt(state)
-
-    def mqtt(self, state):
-        self.oled.write_lines(
-            'WIFI:',
-            ('%8d' % wifi.rssi()) if wifi.is_connected() else ('%8s' % '--'),
-            'MQTT:',
-            '%8s' % ('OK' if state.mqtt else '--'),
-        )
+        i2c = I2C(-1, Pin(pinmap.SCL), Pin(pinmap.SDA))
+        self.oled = SSD1306(i2c)
+        self.update(state, [])
 
     def update(self, state, changed):
-        if 'mqtt' in changed:
-            self.mqtt(state)
-        else:
-            self.oled.write_lines(
-                'SOLR:%3d' % state.solar_temp,
-                'TANK:%3d' % state.tank_temp,
-                'TARG:%3d' % state.tank_target_temp,
-                '%-5s%3s' % (state.mode.upper(), 'ON' if state.pump else 'OFF'),
-            )
+        self.oled.rst()
+        self.oled.txt(
+            'WIFI:%3d' % (wifi.rssi() if wifi.is_connected() else 0)
+        )
+        self.oled.txt(
+            'MQTT:%3s' % ('OK' if hasattr(state, 'mqtt') and state.mqtt else '--')
+        )
+        self.oled.txt(
+            'SOLR:%3d' % state.solar_temp
+        )
+        self.oled.txt(
+            'TANK:%3d' % state.tank_temp
+        )
+        self.oled.txt(
+            'TARG:%3d' % state.tank_target_temp
+        )
+        self.oled.txt(
+            '%-4s:%3s' % (state.mode.upper(), 'ON' if state.pump else 'OFF')
+        )
 
     def stop(self, state):
         self.oled.clear()
